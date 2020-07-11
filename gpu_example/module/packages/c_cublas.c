f802a1a4882fdf271fc60278bcbc1f817f5be610
slnguyen <nguyen.sarah.l@gmail.com>
2020-07-11 00:52:17 -0400

    adding cudaMallocManaged

--- gpu_example/module/packages/c_cublas.c
+++ gpu_example/module/packages/c_cublas.c
@@ -2,28 +2,71 @@
 #include <stdio.h>
 #include  "cublas_v2.h"
 
-//extern proc cublas_saxpy(N: c_int, alpha: c_float, X: []c_float, incX: c_int, Y: []c_float, incY: c_int);
+
+/*
+int cublas_create(cublasHandle_t **handle){
+    *handle = (cublasHandle_t*)malloc(sizeof(cublasHandle_t));
+    return cublasCreate(*handle);
+}
+
+void cublas_destroy(cublasHandle_t *handle){
+    cublasDestroy(*handle);
+}
+
+void *operator new(size_t len){
+    void *ptr;
+    cudaMallocManaged(&ptr, len);
+    cudaDeviceSynchronize();
+    return ptr;
+}
+
+void operator delete(void *ptr){
+    cudaDeviceSynchronize();
+    cudaFree(ptr);
+}
+
+*/
+
+
+void* cublas_array(size_t size){
+    //cudaMallocManaged((void**)&x, size*sizeof(float), 0);
+    //int *a;
+    //int N = 512;
+    //cudaError_t err = cudaMallocManaged(&a,N*sizeof(int), cudaMemAttachGlobal);
+    //cudaError_t err = cudaMallocManaged((void**)&x, size, 0);
+    void *ptr;
+    cudaError_t err = cudaMallocManaged(&ptr, size*sizeof(float), cudaMemAttachGlobal);
+    printf("CUDA Error: %s\n", cudaGetErrorString(err));
+    cudaDeviceSynchronize();
+    return ptr;
+    //return ptr;
+    //for (int i = 0; i < size; i++)
+    //  x[i] = 10;
+    //cudaDeviceSynchronize();
+}
+
 int cublas_saxpy(int N, float alpha, float *x, int incX, float *y, int incY){
 
     cublasHandle_t handle;
 
-    float *d_x, *d_y;
+    //float *d_x, *d_y;
 
     cublasCreate(&handle);
 
-    cudaMalloc((void**)&d_x, N*sizeof(float));
-    cudaMalloc((void**)&d_y, N*sizeof(float));
+    //cudaMalloc((void**)&d_x, N*sizeof(float));
+    //cudaMalloc((void**)&d_y, N*sizeof(float));
 
-    cublasSetVector(N, sizeof(x[0]), x, 1, d_x, 1);
-    cublasSetVector(N, sizeof(y[0]), y, 1, d_y, 1);
+    //cublasSetVector(N, sizeof(x[0]), x, 1, d_x, 1);
+    //cublasSetVector(N, sizeof(y[0]), y, 1, d_y, 1);
 
     // Perform SAXPY on 1M elements
-    cublasSaxpy(handle, N, &alpha, d_x, 1, d_y, 1);
+    //cublasSaxpy(handle, N, &alpha, d_x, 1, d_y, 1);
+    cublasSaxpy(handle, N, &alpha, x, 1, y, 1);
+    cudaDeviceSynchronize();
+    //cublasGetVector(N, sizeof(y[0]), d_y, 1, y, 1);
 
-    cublasGetVector(N, sizeof(y[0]), d_y, 1, y, 1);
-
-    cudaFree(d_x);
-    cudaFree(d_y);
+    //cudaFree(d_x);
+    //cudaFree(d_y);
 
     cublasDestroy(handle);
     return 0;
--- gpu_example/module/packages/cuBLAS.chpl
+++ gpu_example/module/packages/cuBLAS.chpl
@@ -3,6 +3,18 @@ module cuBLAS {
   use C_CUBLAS;
   public use SysCTypes;
 
+  //proc cu_array(X: c_ptr(?eltType), size: size_t){
+  //proc cu_array(X: [?D]?eltType, size: size_t){
+  proc cu_array(size: size_t){
+     require "c_cublas.h", "c_cublas.o";
+     //var X: [1..size] real;
+     var x;
+      //var z = 10;
+     x = cublas_array(size);
+     return x;
+     //cublas_array(X, size);
+  }
+
   proc cu_axpy(X: [?D]?eltType, Y: [D]eltType, ref alpha: eltType, incX: c_int = 1, incY: c_int = 1)
     where D.rank == 1
   {
@@ -18,6 +30,9 @@ module cuBLAS {
 
   module C_CUBLAS {
     use SysCTypes;
+    //extern proc cublas_array(devPtr: []c_float, size: size_t);
+    //extern proc cublas_array(X: c_ptr(c_float), size: size_t);
+    extern proc cublas_array(size: size_t): c_void_ptr;
     extern proc cublas_saxpy(N: c_int, alpha: c_float, X: []c_float, incX: c_int, Y: []c_float, incY: c_int);
   }
 
